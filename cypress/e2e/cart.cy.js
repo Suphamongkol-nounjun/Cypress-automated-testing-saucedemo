27e36b6d21b9c3e3aedf676e021918cfe5a496b3
Suphamongkol Nounjun <115143209+Suphamongkol-nounjun@users.noreply.github.com>
2025-05-20 15:25:35 +0700

    a

--- cypress/e2e/cart.cy.js
+++ cypress/e2e/cart.cy.js
@@ -13,15 +13,26 @@ describe('CART PAGE FUNCTION', () => {
     });
 
     
-    it.only('TC-014: The cart badge should displays the correct number of items currently in the cart',() => {
+    it('TC-014: The cart badge should displays the correct number of items currently in the cart',() => {
         const testCaseId = 'TC-014';
         try {
         loginPage.fillUserPassword(username, password);
         loginPage.clickLoginButton();
         productPage.assertAtProductPage();
 
+                addItems.forEach(item => {
+      productPage[item.method]().then(() => {
+        productPage.getCountCart().then(count => {
+          expect(count).to.equal(item.expectedCount);
+        });
+      });
+    });
+       
         productPage.clickCartIcon();
         cartPage.assertAtCartPage();
+        cartPage.getCountCart().then((count) => {
+            expect(count).to.equal(6);
+        });
 
         cy.task('updateTestResult', { testCaseId, status: 'Pass' });
         } catch (error) {
@@ -31,8 +42,134 @@ describe('CART PAGE FUNCTION', () => {
 
         }
     })
-    // it.only('TC-007: The item name and price in the cart should match the selection from the product page',() => {})
-    // it.only('TC-007: Should remove the selected item from the cart and update the cart badge',() => {})
-    // it.only('TC-007: When clicking "Continue Shopping", should navigates back to the product page',() => {})
-    // it.only('TC-007: When clicking "Checkout", should proceed to the checkout information page',() => {})
+    it('TC-015: The item name and price in the cart should match the selection from the product page',() => {
+        const testCaseId = 'TC-015';
+        try {
+            loginPage.fillUserPassword(username, password);
+            loginPage.clickLoginButton();
+            productPage.assertAtProductPage();
+            
+            const productsArray = [];
+
+productPage.addBackpack().then(product => {
+  productsArray.push(product);
+  return productPage.addBoltTshirt(); // ต้อง return เพื่อให้ Cypress รอ
+}).then(product => {
+  productsArray.push(product);
+
+  productPage.clickCartIcon();
+  cartPage.assertAtCartPage();
+
+  cartPage.getProductNamesAndPrices().then(cartProducts => {
+    cy.log('🛒 สินค้าใน cart:', JSON.stringify(cartProducts));
+    cy.log('📦 สินค้าที่เพิ่ม:', JSON.stringify(productsArray));
+
+    productsArray.forEach(expected => {
+      const found = cartProducts.find(
+        item => item.name.trim() === expected.name.trim() &&
+                item.price.trim() === expected.price.trim()
+      );
+      expect(found, `ต้องเจอสินค้า ${expected.name} ใน cart`).to.not.be.undefined;
+    });
+  });
+});
+
+            cy.task('updateTestResult', { testCaseId, status: 'Pass' });
+        } catch (error) {
+            console.error('Error during login:', error);
+            cy.task('updateTestResult', { testCaseId, status: 'Fail' });
+        }
+    })
+    it('TC-016: Should remove the selected item from the cart and update the cart badge',() => {
+        const testCaseId = 'TC-016';
+        try {
+            loginPage.fillUserPassword(username, password);
+            loginPage.clickLoginButton();
+            productPage.assertAtProductPage();
+
+            productPage.addBackpack().then(() => {
+                productPage.getCountCart().then(count => {
+                    expect(count).to.equal('1');
+                });
+                return productPage.addBikelike();
+            }).then(() => {
+                productPage.getCountCart().then(count => {
+                    expect(count).to.equal('2');
+                });
+                return productPage.clickCartIcon();
+            }).then(() => {
+                cartPage.assertAtCartPage();
+                return cartPage.removeBackpack();
+            }).then(() => {
+                cartPage.getCountCart().then(count => {
+                    expect(count).to.equal(1);
+                });
+            });
+
+            cy.task('updateTestResult', { testCaseId, status: 'Pass' });
+        } catch (error) {
+            console.error('Error during login:', error);
+            cy.task('updateTestResult', { testCaseId, status: 'Fail' });
+        }
+    })
+    it('TC-017: When clicking "Continue Shopping", should navigates back to the product page',() => {
+        const testCaseId = 'TC-017';
+        try {
+            loginPage.fillUserPassword(username, password);
+            loginPage.clickLoginButton();
+            productPage.assertAtProductPage();
+
+            productPage.addBackpack().then(() => {
+                productPage.getCountCart().then(count => {
+                    expect(count).to.equal('1');
+                });
+                return productPage.addBikelike();
+            }).then(() => {
+                productPage.getCountCart().then(count => {
+                    expect(count).to.equal('2');
+                });
+                return productPage.clickCartIcon();
+            }).then(() => {
+                cartPage.assertAtCartPage();
+                return cartPage.clickContinueShopping();
+            }).then(() => {
+                productPage.assertAtProductPage();
+            });
+
+            cy.task('updateTestResult', { testCaseId, status: 'Pass' });
+        } catch (error) {
+            console.error('Error during login:', error);
+            cy.task('updateTestResult', { testCaseId, status: 'Fail' });
+        }
+    })
+    it('TC-018: When clicking "Checkout", should proceed to the checkout information page',() => {
+        const testCaseId = 'TC-018';
+        try {
+            loginPage.fillUserPassword(username, password);
+            loginPage.clickLoginButton();
+            productPage.assertAtProductPage();
+
+            productPage.addBackpack().then(() => {
+                productPage.getCountCart().then(count => {
+                    expect(count).to.equal('1');
+                });
+                return productPage.addBikelike();
+            }).then(() => {
+                productPage.getCountCart().then(count => {
+                    expect(count).to.equal('2');
+                });
+                return productPage.clickCartIcon();
+            }).then(() => {
+                cartPage.assertAtCartPage();
+                return cartPage.clickCheckout();
+            }).then(() => {
+                cy.url().should('include', '/checkout-step-one.html');
+            });
+
+            cy.task('updateTestResult', { testCaseId, status: 'Pass' });
+        } catch (error) {
+            console.error('Error during login:', error);
+            cy.task('updateTestResult', { testCaseId, status: 'Fail' });
+        }
+    })
 });
--- cypress/pages/cart.page.js
+++ cypress/pages/cart.page.js
@@ -1,7 +1,12 @@
 class CartPage {
 
     baseUrl = '/cart.html'
-
+    locatorCountCart = '[data-test="shopping-cart-link"]'
+    locatorClassitemname = '.inventory_item_name'
+    locatorClassitemprice = '.inventory_item_price'
+    locatorRemoveBackpack = '[data-test="remove-sauce-labs-backpack"]'
+    locatorContinueShopping = '[data-test="continue-shopping"]'
+    locatorCheckout = '[data-test="checkout"]'
 
 
   visit() {
@@ -10,6 +15,43 @@ class CartPage {
    assertAtCartPage() {
     cy.url().should('contain', this.baseUrl);
   }
+  getCountCart() {
+    return cy.get(this.locatorCountCart).invoke('text').then(text => {
+      return Number(text.replace(/\D/g, ''));
+    });
+  }
+  removeBackpack() {
+    return cy.get(this.locatorRemoveBackpack).click();
+  }
+  clickContinueShopping() {
+    return cy.get(this.locatorContinueShopping).click();
+  }
+  clickCheckout() {
+    return cy.get(this.locatorCheckout).click();
+  }
+getProductNamesAndPrices() {
+  return cy.get(this.locatorClassitemname).then($names => {
+    const names = [];
+    $names.each((_, el) => {
+      names.push(el.innerText.trim());
+    });
+
+    return cy.get(this.locatorClassitemprice).then($prices => {
+      const prices = [];
+      $prices.each((_, el) => {
+        prices.push(el.innerText.trim());
+      });
+
+      // รวมชื่อกับราคาตาม index
+      const products = names.map((name, index) => ({
+        name,
+        price: prices[index]
+      }));
+
+      return products;
+    });
+  });
+}
 }
 
 
