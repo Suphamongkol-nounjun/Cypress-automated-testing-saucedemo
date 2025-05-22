f4213f2f46ade5a49d4d5ce23003abe64d9d881b
Suphamongkol Nounjun <115143209+Suphamongkol-nounjun@users.noreply.github.com>
2025-05-22 08:48:52 +0700

    update circleci

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 const { updateTestResult } = require('./cypress/utils/updateGoogleSheet');
 
 module.exports = defineConfig({
-  projectId: "4uhssq",
+  projectId: "cm4u1b",
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
     setupNodeEvents(on, config) {
